451d5d5e969b984b42e757f0bb3c1e20cd484f2c
sblackmon <sblackmon@w2odigital.com>
2013-12-13 23:22:55 -1000

    checking in for w2olabs review

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 streams-examples
 ================
+
+twitter-sample-standalone
+-------------------------
+
+java -jar target/twitter-sample-standalone-0.1-SNAPSHOT.jar -Dtwitter.follow.0=18055613 -Dtwitter.track.0=test
+
+twitter-firehose-storm
+----------------------
+
+#TODO
+storm jar target/twitter-firehose-storm-0.1-SNAPSHOT.jar
--- twitter-firehose-storm/src/main/java/org/apache/streams/twitter/example/TwitterFirehoseStorm.java
+++ twitter-firehose-storm/src/main/java/org/apache/streams/twitter/example/TwitterFirehoseStorm.java
@@ -2,40 +2,44 @@ package org.apache.streams.twitter.example;
 
 import com.typesafe.config.Config;
 import org.apache.streams.config.StreamsConfigurator;
-import org.apache.streams.console.ConsolePersistWriter;
-import org.apache.streams.pojo.json.Activity;
+import org.apache.streams.kafka.KafkaConfiguration;
+import org.apache.streams.kafka.KafkaConfigurator;
 import org.apache.streams.twitter.TwitterStreamConfiguration;
 import org.apache.streams.twitter.provider.TwitterStreamConfigurator;
-import org.apache.streams.twitter.provider.TwitterStreamProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Created by sblackmon on 12/10/13.
  */
-public class TwitterSampleStandalone {
+public class TwitterFirehoseStorm {
 
-    private final static Logger LOGGER = LoggerFactory.getLogger(TwitterSampleStandalone.class);
+    private final static Logger LOGGER = LoggerFactory.getLogger(TwitterFirehoseStorm.class);
 
     public static void main(String[] args)
     {
         LOGGER.info(StreamsConfigurator.config.toString());
 
-        Config twitter = StreamsConfigurator.config.getConfig("twitter");
+        Config twitterConfig = StreamsConfigurator.config.getConfig("twitter");
+        Config kafkaConfig = StreamsConfigurator.config.getConfig("kafka");
 
-        TwitterStreamConfiguration twitterStreamConfiguration = TwitterStreamConfigurator.detectConfiguration(twitter);
+        TwitterStreamConfiguration twitterStreamConfiguration = TwitterStreamConfigurator.detectConfiguration(twitterConfig);
+        KafkaConfiguration kafkaConfiguration = KafkaConfigurator.detectConfiguration(kafkaConfig);
 
+        // replace with trident topology create / submit
+        /*
         TwitterStreamProvider stream = new TwitterStreamProvider(twitterStreamConfiguration, Activity.class);
-        ConsolePersistWriter console = new ConsolePersistWriter(stream.getOutQueue());
+        KafkaPersister kafka = new KafkaPersister(stream.getOutQueue());
 
         try {
             Thread provider = (new Thread(stream));
-            Thread persister = (new Thread(console));
+            Thread persister = (new Thread(kafka));
             provider.start();
             persister.start();
         } catch( Exception x ) {
             LOGGER.info(x.getMessage());
         }
+        */
 
         // run until user exits
     }
