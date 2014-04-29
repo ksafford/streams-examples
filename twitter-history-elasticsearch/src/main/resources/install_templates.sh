dfaaf825ae58485b577d5faad01fb9853beda926
sblackmon <sblackmon@w2odigital.com>
2014-04-28 21:03:05 -0500

    tweaks to templates

--- twitter-history-elasticsearch/src/main/resources/install_templates.sh
+++ twitter-history-elasticsearch/src/main/resources/install_templates.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 curl -X PUT 'localhost:9200/_template/base' -d @templates/base.json
+curl -X PUT 'localhost:9200/_template/char_filters' -d @templates/char_filters.json
 curl -X PUT 'localhost:9200/_template/filter' -d @templates/filters.json
 curl -X PUT 'localhost:9200/_template/stop_filter_english' -d @templates/stop_filter_english.json
-curl -X PUT 'localhost:9200/_template/streams_standard_analyzer' -d @templates/streams_standard_analyzer.json
+curl -X PUT 'localhost:9200/_template/streams_keyword_analyzer' -d @templates/streams_keyword_analyzer.json
 curl -X PUT 'localhost:9200/_template/streams_lowercase_analyzer' -d @templates/streams_lowercase_analyzer.json
+curl -X PUT 'localhost:9200/_template/streams_standard_analyzer' -d @templates/streams_standard_analyzer.json
+curl -X PUT 'localhost:9200/_template/streams_tag_analyzer' -d @templates/streams_tag_analyzer.json
 curl -X PUT 'localhost:9200/_template/tweet' -d @templates/tweet.json
 curl -X PUT 'localhost:9200/_template/activity' -d @templates/activity.json
 curl -X PUT 'localhost:9200/_template/twitteractivity' -d @templates/twitteractivity.json
