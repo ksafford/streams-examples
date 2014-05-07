fd3df8d11a5700b9211012dffa25ecfcaeb81379
sblackmon <sblackmon@w2odigital.com>
2014-05-07 14:52:58 -0500

    renamed

--- twitter-userstream-console/src/main/java/org/apache/streams/twitter/example/TwitterUserstreamConsole.java
+++ twitter-userstream-console/src/main/java/org/apache/streams/twitter/example/TwitterUserstreamConsole.java
@@ -3,11 +3,9 @@ package org.apache.streams.twitter.example;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.typesafe.config.Config;
 import org.apache.streams.config.StreamsConfigurator;
+import org.apache.streams.console.ConsolePersistWriter;
 import org.apache.streams.core.StreamBuilder;
 import org.apache.streams.core.StreamsDatum;
-import org.apache.streams.elasticsearch.ElasticsearchConfigurator;
-import org.apache.streams.elasticsearch.ElasticsearchPersistWriter;
-import org.apache.streams.elasticsearch.ElasticsearchWriterConfiguration;
 import org.apache.streams.local.builders.LocalStreamBuilder;
 import org.apache.streams.pojo.json.Activity;
 import org.apache.streams.twitter.TwitterStreamConfiguration;
@@ -22,29 +20,27 @@ import java.util.concurrent.LinkedBlockingQueue;
 /**
  * Created by sblackmon on 12/10/13.
  */
-public class TwitterUserstreamLocal {
+public class TwitterUserstreamConsole {
 
-    private final static Logger LOGGER = LoggerFactory.getLogger(TwitterUserstreamLocal.class);
+    private final static Logger LOGGER = LoggerFactory.getLogger(TwitterUserstreamConsole.class);
 
     public static void main(String[] args)
     {
         LOGGER.info(StreamsConfigurator.config.toString());
 
         Config twitter = StreamsConfigurator.config.getConfig("twitter");
-        Config elasticsearch = StreamsConfigurator.config.getConfig("elasticsearch");
 
         TwitterStreamConfiguration twitterStreamConfiguration = TwitterStreamConfigurator.detectConfiguration(twitter);
-        ElasticsearchWriterConfiguration elasticsearchWriterConfiguration = ElasticsearchConfigurator.detectWriterConfiguration(elasticsearch);
 
         StreamBuilder builder = new LocalStreamBuilder(new LinkedBlockingQueue<StreamsDatum>(100));
 
-        TwitterStreamProvider stream = new TwitterStreamProvider(twitterStreamConfiguration);
-        TwitterTypeConverter converter = new TwitterTypeConverter(ObjectNode.class, Activity.class);
-        ElasticsearchPersistWriter writer = new ElasticsearchPersistWriter(elasticsearchWriterConfiguration);
+        TwitterStreamProvider stream = new TwitterStreamProvider(twitterStreamConfiguration, String.class);
+        TwitterTypeConverter converter = new TwitterTypeConverter(String.class, Activity.class);
+        ConsolePersistWriter console = new ConsolePersistWriter();
 
         builder.newPerpetualStream(TwitterStreamProvider.STREAMS_ID, stream);
         builder.addStreamsProcessor("converter", converter, 2, TwitterStreamProvider.STREAMS_ID);
-        builder.addStreamsPersistWriter(ElasticsearchPersistWriter.STREAMS_ID, writer, 1, "converter");
+        builder.addStreamsPersistWriter("console", console, 1, "converter");
         builder.start();
 
     }
